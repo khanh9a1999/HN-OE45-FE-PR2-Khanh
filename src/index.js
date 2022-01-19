f9ea64052929bbca4fbe83215a85f84aed5419c3
Trương Quốc Khánh <khanh9a1999@gmail.com>
2022-01-19 10:17:56 +0700

    task2-Giaodien-header

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/styles.sass'
+import './i18n'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Suspense fallback="loading...">
+    <React.StrictMode>
+      <Router>
+          <App />
+      </Router>
+    </React.StrictMode>,
+  </Suspense>,
   document.getElementById('root')
 );
-
